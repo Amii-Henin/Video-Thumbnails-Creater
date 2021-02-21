83cc70f705e0ba867db9fbb42b574cc1466c3583
Amii <78695603+Amii-Henin@users.noreply.github.com>
2021-02-21 23:50:51 +0800

    删除已创建的缩略图

---
+++ delete_video_thumb.py
@@ -0,0 +1,70 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+import datetime
+import fractions
+import os
+import re
+import time
+import ffmpeg
+from io import BytesIO
+from PIL import Image, ImageDraw, ImageFont
+Image.MAX_IMAGE_PIXELS = None
+
+alldirs = []
+now = time.strftime('%Y-%m-%d_%H-%M-%S',time.localtime(time.time()))
+
+def main():
+    alldirs.append(rootpath)
+    get_dirs(rootpath)
+    get_dirs_check(alldirs)
+    # print (alldirs)
+    save_log('delete_video_thumb_log.json','a+',now + ':{\n')
+    for path in alldirs:
+        delete_thumb(path)
+        # time.sleep(200)
+    save_log('delete_video_thumb_log.json','a+','}\n')
+
+def get_dirs(root_path):    #遍历目录
+    dirs = os.scandir(root_path)
+    # print (root_path)
+    for x in dirs:
+        if x.is_dir():
+            if x.name != '$RECYCLE.BIN' and x.name != 'System Volume Information':
+                alldirs.append(root_path + '\\' + x.name)
+                get_dirs(root_path + '\\' + x.name)
+
+def get_dirs_check(alldirs):    #遍历目录检查机制
+    if re.search('\\\\\\\\',alldirs[-1]):
+        for x in range(len(alldirs)):
+            temp = alldirs[x].replace('\\\\','\\')
+            alldirs[x] = temp
+
+def delete_thumb(path):        #开始程序
+    files,nfiles,path_files = get_list(path)
+    for x in range(len(files)):
+        pic_name = path + '\\' + nfiles[x] + '_thumb.jpg'
+        if os.path.exists(path + '\\' + nfiles[x] + '_thumb.jpg'):      #判断是否已有缩略图
+            os.remove(pic_name)
+            save_log('delete_video_thumb_log.json','a+','    \"' + pic_name + '\",\n')
+            print ('删除',pic_name)
+
+def get_list(path):     #获取并返回目录下视频文件及目录等
+    all_files = os.listdir(path)
+    rule = r"\.(avi|wmv|wmp|wm|asf|mpg|mpeg|mpe|m1v|m2v|mpv2|mp2v|ts|tp|tpr|trp|vob|ifo|ogm|ogv|mp4|m4v|m4p|m4b|3gp|3gpp|3g2|3gp2|mkv|rm|ram|rmvb|rpm|flv|swf|mov|qt|nsv|dpg|m2ts|m2t|mts|dvr-ms|k3g|skm|evo|nsr|amv|divx|webm|wtv|f4v|mxf)$"
+    path_file_list = []
+    file_list = []
+    nfile_list = []
+    for files in all_files:
+        if re.search(rule, files, re.IGNORECASE):
+            file_list.append(files)
+            nfile_list.append(os.path.splitext(files)[0])
+            path_file_list.append(path + '\\' + files)
+    return (file_list,nfile_list,path_file_list)
+
+def save_log(logname,mode,mess):     #写入日志
+    with open (logname,mode,encoding='utf-8') as f:
+        f.write(mess)
+
+if __name__ == '__main__':
+    rootpath = input('请输入文件夹地址：')
+    main()
